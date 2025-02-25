afa00f601b2f2746e0a175e276de897b2384cdcd
Iazlovitchi Alina <iazlovitchi.alina@isa.utm.md>
2025-02-25 18:45:50 +0200

    Add bundle config

---
+++ UTM.Keto.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Optimization;
+
+namespace UTM.Keto.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // js bundle (без CDN, только локальные файлы в правильном порядке)
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery.min.js",
+                "~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/jquery.mCustomScrollbar.concat.min.js",
+                "~/Scripts/custom.js"
+            ));
+
+            // css bundle (без CDN, только локальные файлы в правильном порядке)
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/style.css",
+                "~/Content/css/responsive.css"
+            ));
+
+            // включение оптимизации (минификация и бандлинг)
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
