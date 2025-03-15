37c454742af20189353db401ca6dedcdd9e21d63
Zaharov Alexandr <zaharov.alexandr@isa.utm.md>
2025-03-16 01:26:45 +0200

    Add reps

---
+++ UTM.Keto.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using UTM.Keto.Domain;
+using UTM.Keto.Domain.Interfaces;
+
+namespace UTM.Keto.Infrastructure.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<IReadOnlyList<User>> GetUsersByRoleAsync(UserRole role)
+        {
+            return await _context.Users
+                .Where(u => u.Role == role)
+                .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<Booking>> GetUserBookingsAsync(Guid userId)
+        {
+            return await _context.Bookings
+                .Where(b => b.UserId == userId)
+                .Include(b => b.Room)
+                .ToListAsync();
+        }
+    }
+}
